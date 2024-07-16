81552f94bd3e871190b3c8334ca7f89d4cc842ae
punithacasi <punithacasi@gmail.com>
2024-07-16 23:11:14 +0200

    Created a new variable
    Objects to the array - contains Pokémon data

---
+++ Js/scripts.js
@@ -0,0 +1,14 @@
+ //This array contains Pokémon data to display in your application.
+
+ /* name - make this a string 
+    height - this will be a number 
+    types - make this an array of strings */
+
+let pokemonList = [
+  { name: 'Bulbasaur', height: 7, types:  ['grass', 'poison']},
+  { name: 'Ivysaur', height: 1, types:  ['grass', 'poison']},
+  { name: 'Venusaur', height: 2, types:  ['grass', 'poison']}
+];
+ 
+
+console.log(pokemonList[1]);
