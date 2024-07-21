1dcbe8590fd60cba411251575bd3620b3517d0a8
punithacasi <punithacasi@gmail.com>
2024-07-21 12:52:24 +0200

    Add Pokémon list
    Added - pokemonList array  - using for loop.
    Added display Label “Wow" text for the biggest List.
    Display using document.write.

--- Js/scripts.js
+++ Js/scripts.js
@@ -1,14 +1,25 @@
- //This array contains Pokémon data to display in your application.
+//This array contains Pokémon data to display in your application.
 
- /* name - make this a string 
-    height - this will be a number 
-    types - make this an array of strings */
+/* name - make this a string 
+   height - this will be a number 
+   types - make this an array of strings */
 
 let pokemonList = [
-  { name: 'Bulbasaur', height: 7, types:  ['grass', 'poison']},
-  { name: 'Ivysaur', height: 1, types:  ['grass', 'poison']},
-  { name: 'Venusaur', height: 2, types:  ['grass', 'poison']}
+  { name: 'Bulbasaur', height: 7, types: ['grass', 'poison'] },
+  { name: 'Ivysaur', height: 1, types: ['grass', 'poison'] },
+  { name: 'Venusaur', height: 2, types: ['grass', 'poison'] }
 ];
- 
 
-console.log(pokemonList[1]);
+
+//“Bulbasaur (height: 7)”.
+//Wow, that’s big!
+for (i = 0; i < pokemonList.length; i++) {
+  let myText = pokemonList[i].name + " (height:" + pokemonList[i].height + ")";
+  if (pokemonList[i].height > 5) {
+    myText += " Wow, that’s big!";
+  }
+  myText += "<br>";
+  document.write(myText);
+
+}
+
