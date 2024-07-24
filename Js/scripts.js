4b4faa4beaaeb24844c2b2ffb301eba187000b2c
punithacasi <punithacasi@gmail.com>
2024-07-25 01:12:00 +0200

    Modified for IIFE immplimentation
    Modified for IIFE immplimentation

--- Js/scripts.js
+++ Js/scripts.js
@@ -4,29 +4,37 @@
    height - this will be a number 
    types - make this an array of strings */
 
-   let pokemonList = [
-    { name: 'Bulbasaur', height: 7, types: ['grass', 'poison'] },
-    { name: 'Ivysaur', height: 1, types: ['grass', 'poison'] },
-    { name: 'Venusaur', height: 2, types: ['grass', 'poison'] }
-  ];
   
-  
-  //“Bulbasaur (height: 7)”.
-  //Wow, that’s big!
+let pokemonRepository = (function () {
+  let pokemonList = [];
 
+  function add(pokemon) {
+    pokemonList.push(pokemon);
+  }
+
+  function getAll() {
+    return pokemonList;
+  }
+
+  return {
+    add: add,
+    getAll: getAll
+  };
+})();
+
+pokemonRepository.add({ name: 'Bulbasaur', height: 7, types: ['grass', 'poison'] });
+pokemonRepository.add({ name: 'Ivysaur', height: 1, types: ['grass', 'poison'] });
+pokemonRepository.add({ name: 'Venusaur', height: 2, types: ['grass', 'poison'] });
+console.log(pokemonRepository.getAll());
 
 //ForEach
-pokemonList.forEach(function(item){
+pokemonRepository.getAll().forEach(function(item){
   let myText = item.name + " (height:" + item.height + ")";
   if (item.height > 5) {
     myText += " Wow, that’s big!";
   }
   myText += "<br>";
   document.write(myText);
-
-
 });
 
-
-  
-  
+modified for IIFE immplimentation
