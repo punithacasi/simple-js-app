2610ac3d8e8938a3e4143e4c46426367a50fe09d
punithacasi <punithacasi@gmail.com>
2024-08-04 22:45:04 +0200

    update with Ajax
    Added polyfills for promises and fetch
    
    promise-polyfill.js
    fetch-polyfills.js
    
    index.html
    -----------
    Include polyfills for promises and fetch js
    Remove the array of Pokémon objects.
    Created a <p> to display a loading message
    
    script.js
    ----------
    Added a function  LoadList()
    Added a function loadDetails()
    
    Assign imgUrl and height to pokemonList
    
    LoadList() and loadDetails()  assigned to keys  - returned object  -pokemonRepository.
    
    Created a function to display a loading message
    showLoadingMessage() and hideLoadingMessage()
    
    style.css
    ----------
    Added a class .pokemon-list to display grid

--- Js/scripts.js
+++ Js/scripts.js
@@ -2,6 +2,8 @@
 let pokemonRepository = (function () {
   let pokemonList = [];
 
+  let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=20';
+
   function add(pokemon) {
     pokemonList.push(pokemon);
   }
@@ -9,9 +11,70 @@ let pokemonRepository = (function () {
   function getAll() {
     return pokemonList;
   }
+
+ 
+  function showLoadingMessage(){ 
+    let loading = document.querySelector(".loading");
+    loading.innerText = "Loading data...";
+    loading.classList.add("is-visible");
+
+}
+
+function hideLoadingMessage(){
+let loading = document.querySelector(".loading");
+loading.innerText = "";
+loading.classList.remove("is-visible");
+
+}
+
+
+
+  //function Load all 
+
+  function loadList() {
+    showLoadingMessage();   
+    return fetch(apiUrl).then(function (response) {
+      return response.json();
+    }).then(function (json) {
+      hideLoadingMessage();
+      json.results.forEach(function (item) {
+        let pokemon = {
+          name: item.name,
+          detailsUrl: item.url
+        };
+        add(pokemon);
+      });
+    }).catch(function (e) {
+      hideLoadingMessage();
+      console.error(e);
+    })
+  }
+
+  function loadDetails(pokemon){
+    showLoadingMessage();   
+    return fetch(pokemon.detailsUrl).then(function (response) {
+       //console.log(response);
+        return response.json();
+    
+    }).then(function (json) {
+      hideLoadingMessage();
+          pokemon.image = json.sprites.front_default;
+          pokemon.height = json.height;
+         
+        console.log(pokemon);  
+        return pokemon;
+  }).catch(function (e) {
+    hideLoadingMessage();
+      console.error(e);
+    })
+  }
+ 
+
+
   // function to display Pokemon name in console
   function showDetails(pokemon) {
-    console.log(pokemon.name);
+    console.log(pokemon);
+    loadDetails(pokemon); 
   }
 
   // function to add click event (addEventListener) 
@@ -33,24 +96,26 @@ let pokemonRepository = (function () {
     elementContainer.appendChild(elementList);
   }
 
+ 
+
   return {
     add: add,
     getAll: getAll,
-    addListItem: addListItem
-
+    addListItem: addListItem,
+    loadList:loadList,
+    loadDetails:loadDetails,
+    showDetails:showDetails
   };
 })();
 
+ 
+//console.log(pokemonRepository.getAll());
 
-
-
-pokemonRepository.add({ name: 'Bulbasaur', height: 7, types: ['grass', 'poison'] });
-pokemonRepository.add({ name: 'Ivysaur', height: 1, types: ['grass', 'poison'] });
-pokemonRepository.add({ name: 'Venusaur', height: 2, types: ['grass', 'poison'] });
-console.log(pokemonRepository.getAll());
-
-//ForEach
-pokemonRepository.getAll().forEach(function (item) {
-  pokemonRepository.addListItem(item);
+// assigned to keys with the same name in the returned objec
+ 
+pokemonRepository.loadList().then(function() {
+  // Now the data is loaded!
+  pokemonRepository.getAll().forEach(function(pokemon){
+    pokemonRepository.addListItem(pokemon);
+  });
 });
-
